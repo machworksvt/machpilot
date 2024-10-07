b2caeb09a14fcd02d1c029b405cb04e35f4c7e5f
natecvt <natec@vt.edu>
2024-10-07 13:30:36 -0400

    Completed BNO055 class definition

--- src/ros-imu-bno055/src/bno055_i2c_node.cpp
+++ src/ros-imu-bno055/src/bno055_i2c_node.cpp
@@ -32,5 +32,61 @@ private:
 };
 
 BNO055::BNO055() : Sensor("BNO055_Node") {
+    this->declare_parameter<std::string>("device", "/dev/i2c-1");
     this->declare_parameter<int>("address", BNO055_ADDRESS_A);
+    this->declare_parameter<std::string>("frame_id", "imu");
+    this->declare_parameter<double>("rate", 100.0);
+
+    this->get_parameter("device", param_device);
+    this->get_parameter("address", param_address);
+    this->get_parameter("frame_id", param_frame_id);
+    this->get_parameter("rate", param_rate);
+
+    imu = std::make_unique<imu_bno055::BNO055I2CDriver>(param_device, param_address);
+
+    pub_data = this->create_publisher<sensor_msgs::msg::Imu>("data", 10);
+    pub_raw = this->create_publisher<sensor_msgs::msg::Imu>("raw", 10);
+    pub_mag = this->create_publisher<sensor_msgs::msg::MagneticField>("mag", 10);
+    pub_temp = this->create_publisher<sensor_msgs::msg::Temperature>("temp", 10);
+    pub_status = this->create_publisher<diagnostic_msgs::msg::DiagnosticStatus>("status", 10);
+
+    srv_reset = this->create_service<std_srvs::srv::Trigger>("init", std::bind(&BNO055::initSrvs, this, std::placeholders::_1, std::placeholders::_2));
+
+    seq = 0;
+
+    current_status.level = 0;
+    current_status.name = "BNO055 IMU";
+    current_status.hardware_id = "bno055_i2c";
+
+    diagnostic_msgs::msg::KeyValue calib_stat;
+    calib_stat.key = "Calibration status";
+    calib_stat.value = "";
+    current_status.values.push_back(calib_stat);
+
+    diagnostic_msgs::msg::KeyValue selftest_result;
+    selftest_result.key = "Self-test result";
+    selftest_result.value = "";
+    current_status.values.push_back(selftest_result);
+
+    diagnostic_msgs::msg::KeyValue intr_stat;
+    intr_stat.key = "Interrupt status";
+    intr_stat.value = "";
+    current_status.values.push_back(intr_stat);
+
+    diagnostic_msgs::msg::KeyValue sys_clk_stat;
+    sys_clk_stat.key = "System clock status";
+    sys_clk_stat.value = "";
+    current_status.values.push_back(sys_clk_stat);
+
+    diagnostic_msgs::msg::KeyValue sys_stat;
+    sys_stat.key = "System status";
+    sys_stat.value = "";
+    current_status.values.push_back(sys_stat);
+
+    diagnostic_msgs::msg::KeyValue sys_err;
+    sys_err.key = "System error";
+    sys_err.value = "";
+    current_status.values.push_back(sys_err);
+
+    rate = std::make_unique<rclcpp::Rate>(param_rate);
 }
