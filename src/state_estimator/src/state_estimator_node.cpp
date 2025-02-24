303741ba940d1e9a4984624bbede8b7597d99724
natecvt <natec@vt.edu>
2025-02-24 18:14:52 -0500

    Changed files around again to get node to build

--- src/state_estimator/src/state_estimator_node.cpp
+++ src/state_estimator/src/state_estimator_node.cpp
@@ -1,8 +1,22 @@
 #include "controller.hpp"
 #include "rc/math/math.h"
-#include "rc/time/time.h"
+#include "rc/time.h"
 
-class STATE_ESTMATOR : public Controller
+class STATE_ESTMATORNode : public Controller
 {
+public:
+    STATE_ESTMATORNode() : Controller("state_estimator_node") {
 
+    }
 };
+
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<STATE_ESTMATORNode>());
+  rclcpp::shutdown();
+  return 0;
+}
