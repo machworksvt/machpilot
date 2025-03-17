c49888352310c6887469e1f74c491de2ee5aa2f0
natecvt <natec@vt.edu>
2025-03-17 16:56:20 -0400

    Added stuff to the state estimator for the IMU, starting the KF implementation

--- src/state_estimator/include/state_estimator/state_estimator.h
+++ src/state_estimator/include/state_estimator/state_estimator.h
@@ -1,5 +1,6 @@
 #include "controller.hpp"
 #include "rc/math/vector.h"
+#include "rc/math/matrix.h"
 
 // a store for raw data incoming from sensors
 typedef struct {
@@ -32,6 +33,16 @@ typedef struct {
     double imu_time_2;
     rc_vector_t imu_omega_1;
     rc_vector_t imu_omega_2;
+    rc_matrix_t imu_var_o_1;
+    rc_matrix_t imu_var_o_2;
+    rc_vector_t imu_accel_1;
+    rc_vector_t imu_accel_2;
+    rc_matrix_t imu_var_a_1;
+    rc_matrix_t imu_var_a_2;
+    rc_vector_t imu_quat_1;
+    rc_vector_t imu_quat_2;
+    rc_matrix_t imu_var_t_1;
+    rc_matrix_t imu_var_t_2;
 } raw_state_t;
 
 // a store for filtered data incoming from sensors and current control outputs
