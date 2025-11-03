7daf23391e1859df2ba9805f059406aa384a5e4a
Aldo Hernandez-Mendez <aldohernandez-mendez@Shi-Anns-MacBook-Air.local>
2025-11-03 10:48:38 -0500

    fixed algorithim

--- src/embedded_system/ublox_gps_driver/src/ublox_messages.cpp
+++ src/embedded_system/ublox_gps_driver/src/ublox_messages.cpp
@@ -6,9 +6,11 @@ namespace ublox {
         const uint8_t* data,
         size_t length
     ){
+        
+        // 8-bit Fletcher algorithm
         uint8_t ck_a = 0;
         uint8_t ck_b = 0;
-        for (size_t i = 0; i < length ) {
+        for (size_t i = 0; i < length, i++) {
             ck_a = (ck_a + data[i]) & 0xFF;
             ck_b = (ck_b + ck_a) & 0xFF;
         }
