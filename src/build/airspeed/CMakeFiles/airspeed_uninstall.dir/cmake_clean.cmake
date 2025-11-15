0d195efe045812fc685279944721074430228ade
Aldo Hernandez-Mendez <aldohernandez-mendez@Shi-Anns-MacBook-Air.local>
2025-11-14 20:34:03 -0500

    remove build folder

--- src/build/airspeed/CMakeFiles/airspeed_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/airspeed_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/airspeed_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
